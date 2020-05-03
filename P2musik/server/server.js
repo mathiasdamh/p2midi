8653d48f984fa36cdbd2543e9de4c584784708d5
Winkelsan <62054714+Winkelsan@users.noreply.github.com>
2020-05-03 20:45:27 +0200

    hey

--- P2musik/server/server.js
+++ P2musik/server/server.js
@@ -1,25 +1,35 @@
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
+// Dette sørger for at de installerede biblioteker bliver inkluderet. plus serveren bliver startet ved http.
+let express = require("express"),
+    app = express(),
+    http = require("http").Server(app).listen(8080),
+    upload = require("express-fileupload");
+//// denne funktion sørger for at der er mulighed for at uploade.
+app.use(upload())
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/Niklas/Desktop/webdev/P2musik/SavedFiles/img' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-}).listen(8080);
+console.log("Server Started")
+
+// Dette sørger for at filer som css bliver brugt, når serveren starter op. (Måske ikke nødvendig)
+app.use(express.static('public'));
+
+// laver en get request på filen upload.html, som gør at filen kan ses i browseren når serveren laver en get request!
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/upload.html");
+})
+
+// Her bliver der brugt noget fra express biblioteket, som sørger for at en fil bliver uploadet et bestemt sted i mappen, ved hjælp
+// af en post request, som sender det til serveren.
+app.post("/",function(req,res){
+    if(req.files){
+        let file = req.files.filename,
+            filename = file.name;
+        file.mv("C:/Users/Niklas/Documents/GitHub/p2midi/P2musik/server/upload/"+filename,function(err){
+            if(err){
+                console.log(err)
+                res.send("Error Occured")
+            } else{
+                console.log("File Uploaded");
+                res.send("File Uploaded");
+            }
+        })
+    }
+})
